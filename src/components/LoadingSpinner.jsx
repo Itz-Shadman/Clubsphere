7f2380dfaf9fbe78cb220eca03bd223e65197659
Itz-Shadman <chowdhuryshadman707@gmail.com>
2025-12-25 11:52:54 +0000

    fourth commit

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ const LoadingSpinner = () => {
     return (
         <div className="flex justify-center items-center h-screen bg-base-200">
             <div className="flex flex-col items-center space-y-4">
-                {/* DaisyUI spinner */}
+
                 <span className="loading loading-spinner loading-lg text-clubsphere-primary"></span>
                 <p className="text-lg font-semibold text-gray-700 animate-pulse">
                     ClubSphere is loading...
